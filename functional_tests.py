153f369a24bdc9c6c07c124382e7fe522d7db5fb
albert <xins@bupt.edu.cn>
2022-05-03 21:17:24 +0800

    supplemention front page html ...

--- functional_tests.py
+++ functional_tests.py
@@ -33,8 +33,8 @@ class NewVisitorTest(unittest.TestCase):
 
 
 	
-	if __name__ == '__main__':
-		unittest.main(warnings='ignore')
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
 
 
 
