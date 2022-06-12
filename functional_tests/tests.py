5af59187559b49351d470dd44589a50f228b8126
albert <xins@bupt.edu.cn>
2022-06-12 14:53:06 +0800

    fix database null key

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,87 +5,88 @@ from selenium.common.exceptions import WebDriverException
 import time
 
 class NewVisitorTest(LiveServerTestCase):
-	def setUp(self):
-		self.browser = webdriver.Firefox()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-	def tearDown(self):
-		self.browser.quit()
-		
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
+    def tearDown(self):
+        self.browser.quit()
+        
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
-	def wait_for_row_in_list_table(self, row_text):
-		MAX_WAIT = 10
-		start_time = time.time()
-		while True:
-			try:
-				table = self.browser.find_element_by_id('id_list_table')
-				rows = table.find_elements_by_tag_name('tr')
-				self.assertIn(row_text, [row.text for row in rows])
-				return
-			except(AssertionError, WebDriverException) as e:
-				if time.time() - start_time > MAX_WAIT:
-					raise e
-				time.sleep(0.5)
+    def wait_for_row_in_list_table(self, row_text):
+        MAX_WAIT = 8
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except(AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
  
-	def test_can_start_a_list_for_one_user(self):
-		self.browser.get(self.live_server_url)
-		
-		self.assertIn('To-Do', self.browser.title)
-		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('To-Do', header_text)
-		
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Buy peacock feathers')
-		inputbox.send_keys(Keys.ENTER)
-		self.wait_for_row_in_list_table('1: Buy peacock feathers')
-		
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Use peacock fethers to make a fly')
-		inputbox.send_keys(Keys.ENTER)
-		
-		self.wait_for_row_in_list_table('1: Buy peacock feathers')
-		self.wait_for_row_in_list_table('2: Use peacock fethers to make a fly')
-		
-		
-	def test_mutiple_users_can_start_lists_at_different_url(self):
-		self.browser.get(self.live_server_url)
-			
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Buy peacock feathers')
-		inputbox.send_keys(Keys.ENTER)
-		
-		self.wait_for_row_in_list_table('1: Buy peacock feathers')
-	
-		edith_list_url = self.browser.current_url
-		self.assertRegex(edith_list_url, '/lists/.+')
-		
-	
-		self.browser.quit()
-		self.browser = webdriver.Firefox()
-		
-		self.browser.get(self.live_server_url)
-		page_text = self.browser.find_element_by_tag_name('body').text
-	
-		self.assertNotIn('Buy peacock feathers', page_text)
-		self.assertNotIn('make a fly', page_text)
-	
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Buy mulk')
-		inputbox.send_keys(Keys.ENTER)
-		self.wait_for_row_in_list_table('1: Buy mulk')
-	
-		francis_list_url = self.browser.current_url
-		self.assertRegex(francis_list_url, '/lists/.+')
-		self.assertNotEqual(francis_list_url, edith_list_url)
-	
-		page_text = self_browser.find_element_by_tag_name('body').text
-		self.assertNotIn('Buy peacock feathers', page_text)
-		self.assertIn('Buy mulk', page_text)
-
-
+    def test_can_start_a_list_for_one_user(self):
+        self.browser.get(self.live_server_url)
+        
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock fethers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock fethers to make a fly')
+        
+        
+    def test_mutiple_users_can_start_lists_at_different_url(self):
+        self.browser.get(self.live_server_url)
+            
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+    
+        edith_list_url = self.browser.current_url
+        
+        self.assertRegex(edith_list_url, '/lists/.+')
+        self.browser.quit()
+        
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.live_server_url)
+        
+        page_text = self.browser.find_element_by_tag_name('body').text
+    
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+    
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy mulk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy mulk')
+    
+        francis_list_url = self.browser.current_url
+        
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+    
+        page_text = self_browser.find_element_by_tag_name('body').text
+        
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy mulk', page_text)
+        self.browser.quit()
 
 browser = webdriver.Firefox()
 # Edith has heard about a cool new online to-do app. She goes to check out its homepage
--- lists/tests.py
+++ lists/tests.py
@@ -1,69 +1,116 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
-
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
-	def test_uses_home_template(self):
-		response = self.client.get('/')
-		self.assertTemplateUsed(response, 'home.html')
 
-	def test_can_save_a_POST_request(self):
-		response = self.client.post('/', data={'item_text': 'A new list item'})
-		
-		self.assertEqual(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, 'A new list item')
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-	def test_redirects_after_POST(self):
-		response = self.client.post('/', data={'item_text': 'A new list item'})
-		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')		
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 
 class ItemModelTest(TestCase):
-	
-	def test_saving_and_retrieving_items(self):
-		first_item = Item()
-		first_item.text = 'The first (ever) list item'
-		first_item.save()
+    
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
 
-		second_item = Item()
-		second_item.text = 'Item the second'
-		second_item.save()
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
 
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(), 2)
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
 
-		first_saved_item = saved_items[0]
-		second_saved_item = saved_items[1]
-		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-		self.assertEqual(second_saved_item.text, 'Item the second')
-		
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        
 class ListViewTest(TestCase):
-		
-	def test_displays_all_list_items(self):
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
-		
-		response = self.client.get('/lists/the-only-list-in-the-world/')
+        
+    def test_displays_all_list_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
+        
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
-		self.assertContains(response, 'itemey 1')
-		self.assertContains(response, 'itemey 2')
-	
-	def test_uses_list_template(self):
-		response = self.client.get('/lists/the-only-list-in-the-world/')
-		self.assertTemplateUsed(response, 'list.html')
-	
-	def test_can_save_a_POST_request(self):
-		self.client.post('/lists/new', data={'item_text': 'A new list item'})
-		
-		self.assertEqual(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, 'A new list item')
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+    
+    def test_uses_list_template(self):
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
+        self.assertTemplateUsed(response, 'list.html')
 
-	def test_redirects_after_POST(self):
-		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')				
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other itemey 1', list=other_list)
+        Item.objects.create(text='other itemey 2', list=other_list)
+        
+        response = self.client.get(f'/lists/{correct_list.id}/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other itemey 1')
+        self.assertNotContains(response, 'other itemey 2')
+        
+    
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')                
+
+class ListAndItemModelsTest(TestCase):
+    
+    def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
+        first_item = Item()
+        first_item.text = 'The first(ever) list item'
+        first_item.list = list_
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'The second item'
+        second_item.list = list_
+        second_item.save()
+        
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first(ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
+
+        self.assertEqual(second_saved_item.text, 'The second item')
+        self.assertEqual(second_saved_item.list, list_)
