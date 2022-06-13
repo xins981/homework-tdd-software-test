ba8df2b3da94a66ea1a771a0a9129f310ab98728
albert <xins@bupt.edu.cn>
2022-06-13 16:16:33 +0800

    refactor template to use the base.html

--- lists/urls.py
+++ lists/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from lists import views
 
 urlpatterns = [
-	url(r'^new$', views.new_list, name='new_list'),
+	url(r'^new/$', views.new_list, name='new_list'),
 	url(r'^(\d+)/$', views.view_list, name='view_list'),
-	url(r'^(\d+)/add_item$', views.add_item, name='add_list'),
+	url(r'^(\d+)/add_item/$', views.add_item, name='add_list'),
 ]
